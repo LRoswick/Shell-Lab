8f082a93358fb45fd93c8ec4cdb95422e5e6d374
Luke Howard Roswick <lroswick@data.cs.purdue.edu>
2022-10-18 16:11:42 -0400

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -125,7 +125,7 @@ void Command::execute() {
         //child
 	
 	//may need to be strings
-	execvp(_simpleCommands[i]->_arguments[0], _simpleCommands[i]->_arguments);
+	execvp((char * )_simpleCommands[i]->_arguments[0], _simpleCommands[i]->_arguments);
 
 	perror("execvp");
 	exit(1);
