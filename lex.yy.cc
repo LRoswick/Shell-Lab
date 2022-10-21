486cc76a417f99d7ff0c7103041d7497b8cd9b28
Luke Howard Roswick <lroswick@data.cs.purdue.edu>
2022-10-21 17:16:21 -0400

    \Part 2 Total:            5.5 of 22.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -941,7 +941,8 @@ YY_RULE_SETUP
   char buffer[1025];
   //read from pout[0]
   int string = read(pout[0], buffer, 1024);  
-  for (int i = string -12; i >= 0; --i) {
+  for (int i = string -12; i >= 0; i--) {
+    printf("%d", buffer[i]);
     if (buffer[i] == '\n') {
       myunputc('\t');
     } else {
@@ -960,7 +961,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 145 "shell.l"
+#line 146 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -969,10 +970,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 150 "shell.l"
+#line 151 "shell.l"
 ECHO;
 	YY_BREAK
-#line 976 "lex.yy.cc"
+#line 977 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1989,4 +1990,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 150 "shell.l"
+#line 151 "shell.l"
