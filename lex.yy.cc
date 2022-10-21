6687936ccc99cd1c10b92b689295d0042cf62004
Luke Howard Roswick <lroswick@data.cs.purdue.edu>
2022-10-21 16:19:04 -0400

    \Part 2 Total:            5.5 of 22.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -919,7 +919,11 @@ YY_RULE_SETUP
 
   int ret = fork();
   if (ret == 0) {
-    const char ** command[2] = { "/proc/self/exe", NULL};
+    const char * command[2];     //= { "/proc/self/exe", NULL};
+    command[0] = "/proc/self/exe";
+    command[1] = NULL;
+
+
     execvp(command[0], command);
   }
   
@@ -930,7 +934,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 115 "shell.l"
+#line 119 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -939,10 +943,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 120 "shell.l"
+#line 124 "shell.l"
 ECHO;
 	YY_BREAK
-#line 946 "lex.yy.cc"
+#line 950 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1959,4 +1963,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 120 "shell.l"
+#line 124 "shell.l"
