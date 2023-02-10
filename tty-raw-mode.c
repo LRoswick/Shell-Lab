be43400e07d6967144810bb827c8e51a596d1a57
LRoswick <70717563+LRoswick@users.noreply.github.com>
2023-02-10 15:13:11 -0500

    Update tty-raw-mode.c

--- tty-raw-mode.c
+++ tty-raw-mode.c
@@ -4,13 +4,6 @@
 #include <termios.h>
 #include <string.h>
 
-/* 
- * Sets terminal into raw mode. 
- * This causes having the characters available
- * immediately instead of waiting for a newline. 
- * Also there is no automatic echo.
- */
-
 void tty_raw_mode(void)
 {
 	struct termios tty_attr;
